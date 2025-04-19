f5b19475471324c70a48edf1c4ca7dde566eeb7b
sampath routhu <dssrouthu@gmail.com>
2025-04-19 13:10:25 -0400

    Updated Books contract and scripts

--- script/Books.s.sol
+++ script/Books.s.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+import "forge-std/Script.sol";
+import "../src/Books.sol";
+
+contract DeployBooks is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        new Books("Programming Foundry", "sampath routhu", 100);
+        vm.stopBroadcast();
+    }
+}
